860e22e9761b9e2ff03949ec71525d2827df249a
Fouzul Hassan <fouzul.hassan@aiesec.net>
2024-04-04 15:56:55 +0530

    feat: leaderboard

--- leaderboard.py
+++ leaderboard.py
@@ -95,18 +95,6 @@ def count_approved_by_entity(df, selected_function):
 
 icon_path = 'https://aiesec.lk/data/dist/images/favicon.png'
 
-entity_colors={
-                'CC': '#ffdabc',
-                'CN': '#cfbaf0',
-                'CS': '#90dbf4',
-                'USJ': '#efeaa9',
-                'Kandy': '#a3c4f3',
-                'Ruhuna': '#a6f2ae',
-                'SLIIT': '#f1c0e8',
-                'NSBM': '#8eecf5',
-                'NIBM': '#98f5e1',
-                'Rajarata': '#ffcfd2'
-            }
 
 def show_guide():
 
@@ -122,11 +110,42 @@ def show_guide():
     dark_gif = open("dark.gif", "rb").read()
     st.image(dark_gif)
 
+def calculate_approval_ranks(df):
+    # Sort the DataFrame by 'Total_Approved' column in descending order
+    df_sorted = df.sort_values(by='Total_Approved', ascending=False)
+    
+    # Add a new column 'Rank' to store the ranks
+    df_sorted['Rank'] = range(1, len(df_sorted) + 1)
+    
+    return df_sorted
+
+
+def display_approval_ranks(df):
+    # Calculate ranks
+    df_with_ranks = calculate_approval_ranks(df)
+    
+    # Drop the index column
+    df_without_index = df_with_ranks[['Rank', 'Entity', 'Total_Approved']]
+    # Rename the column 'Total_Approved' to 'Total Approvals'
+    df_with_ranks.rename(columns={'Total_Approved': 'Total Approvals'}, inplace=True)
+
+    
+    # Apply gold, silver, and bronze medals to the 'Entity' column
+    df_with_ranks['Entity'] = df_with_ranks.apply(lambda row: 
+                                                   f"🥇 {row['Entity']}" if row['Rank'] == 1 else 
+                                                   f"🥈 {row['Entity']}" if row['Rank'] == 2 else 
+                                                   f"🥉 {row['Entity']}" if row['Rank'] == 3 else 
+                                                   row['Entity'], axis=1)
+    
+    # Display the DataFrame with medals
+    st.subheader('Leaderboard')
+    st.dataframe(df_with_ranks.set_index('Rank'), use_container_width=True, height=250)
+
 
 # Main Streamlit app
 def main():
     st.set_page_config(
-    layout="wide",
+    layout="centered",
     page_title="AP Hackathon - Dashboard",
     page_icon= icon_path,
     )   
@@ -152,24 +171,6 @@ def main():
         # Check if the 'Entity' column exists in the DataFrame
         if 'Entity' in data.columns:
 
-            # Create a sidebar with a selector to choose the 'Function'
-            #selected_function = st.sidebar.selectbox('Select Function', data['Function'].unique())
-
-            
-            # Calculate entity sum
-            # entity_sum = calculate_entity_sum(data)
-
-            # Convert entity sum to JSON object
-            # entity_sum_json = json.dumps(entity_sum)
-
-            # # Create the bar chart
-            # bar_chart = create_bar_chart(entity_sum)
-
-            # Display the bar chart using Plotly Chart
-            # st.plotly_chart(bar_chart, use_container_width=True)
-
-
-            # Barchart 1 : APP
             # Calculate total 'Applied' related to each entity
             entity_applied_total = calculate_total_applied(data)
 
@@ -178,13 +179,15 @@ def main():
             df_entity_applied_total.reset_index(inplace=True)
             df_entity_applied_total.rename(columns={'index': 'Entity'}, inplace=True)
 
+            
             # Create a colored bar chart using Plotly Express
-            fig = px.bar(df_entity_applied_total, x='Entity', y='Total_Applied', title='Total Applications by Entity', labels={'Entity': 'Entity', 'Total_Applied': 'Applications'}, color='Entity', color_discrete_map=entity_colors)
+            fig = px.bar(df_entity_applied_total, x='Entity', y='Total_Applied', labels={'Entity': 'Entity', 'Total_Applied': 'Applications'}, color='Entity')
 
             # Hide the legend
             fig.update_layout(showlegend=False)
 
-
+            # Use the function to display the ranks table
+            
             # Barchart 2: APD
             # Calculate total 'Approved' related to each entity
             entity_approved_total = calculate_total_approved(data)
@@ -193,51 +196,17 @@ def main():
             df_entity_approved_total = pd.DataFrame.from_dict(entity_approved_total, orient='index', columns=['Total_Approved'])
             df_entity_approved_total.reset_index(inplace=True)
             df_entity_approved_total.rename(columns={'index': 'Entity'}, inplace=True)
-
+            display_approval_ranks(df_entity_approved_total)
             # Create a colored bar chart using Plotly Express
-            fig_approved = px.bar(df_entity_approved_total, x='Entity', y='Total_Approved', title='Total Approvals by Entity', labels={'Entity': 'Entity', 'Total_Approved': 'Approvals'},color='Entity', color_discrete_map=entity_colors)
-
+            fig_approved = px.bar(df_entity_approved_total, x='Entity', y='Total_Approved', title='Total Approvals by Entity', labels={'Entity': 'Entity', 'Total_Approved': 'Approvals'},color='Entity')
             # Hide the legend
             fig_approved.update_layout(showlegend=False)
 
-            # Barchart 3: Unique LCs
-            # Calculate total 'Unique_LCs' related to each entity
-            # entity_unique_lcs_total = calculate_total_unique_lcs(data)
-
-            # # Convert dictionary to DataFrame
-            # df_entity_unique_lcs_total = pd.DataFrame.from_dict(entity_unique_lcs_total, orient='index', columns=['Total_Unique_LCs'])
-            # df_entity_unique_lcs_total.reset_index(inplace=True)
-            # df_entity_unique_lcs_total.rename(columns={'index': 'Entity'}, inplace=True)
-
-            # # Create a colored bar chart using Plotly Express
-            # fig_unique_lcs = px.bar(df_entity_unique_lcs_total, x='Entity', y='Total_Unique_LCs', title='Total Unique LCs by Entity', labels={'Entity': 'Entity', 'Total_Unique_LCs': 'Unique LCs'},color='Entity', color_discrete_map=entity_colors)
-
-            # # Hide the legend
-            # fig_unique_lcs.update_layout(showlegend=False)
-
-            # Display the bar charts using Plotly Chart
-            # col1, col2, col3 = st.columns(3)
-            # col1, col2 = st.columns(2)
-
-            # with col1:
-            #     # Render the bar chart using Streamlit
-            #     st.plotly_chart(fig, use_container_width=True)
-
-            # with col2:
-            #     # Render the bar chart using Streamlit
-            #     st.plotly_chart(fig_approved, use_container_width=True)
-
-            # with col3:
-            #     # Render the bar chart using Streamlit
-            #     st.plotly_chart(fig_unique_lcs, use_container_width=True)
             st.plotly_chart(fig_approved, use_container_width=True)
             st.plotly_chart(fig, use_container_width=True)
             
 
             st.subheader('Functional Analysis')
-
-            # Generate and display grouped bar chart
-            #st.plotly_chart(generate_grouped_bar_chart(data, selected_entity))
             # Create a select box to choose the 'Function'
             selected_function = st.selectbox('Select Function', data['Function'].unique())
             
@@ -247,45 +216,23 @@ def main():
             applied_counts = count_applied_by_entity(data, selected_function)
 
             # Create a bar chart using Plotly Express
-            fig_1 = px.bar(applied_counts, x='Entity', y='Count_Applied', title=f'Applications by Entity for {selected_function} Function',labels={'Entity': 'Entity', 'Count_Applied': 'Applications'}, color='Entity', color_discrete_map=entity_colors)
+            fig_1 = px.bar(applied_counts, x='Entity', y='Count_Applied', title=f'Applications by Entity for {selected_function} Function',labels={'Entity': 'Entity', 'Count_Applied': 'Applications'}, color='Entity')
             fig_1.update_layout(showlegend=False)
             # Barchart 5: APD by Function
             # Get the count of 'Approved' related to each entity based on the selected function
             approved_counts = count_approved_by_entity(data, selected_function)
 
             # Create a bar chart using Plotly Express
-            fig_2 = px.bar(approved_counts, x='Entity', y='Count_Approved', title=f'Approvals by Entity for {selected_function} Function',labels={'Entity': 'Entity', 'Count_Approved': 'Approvals'}, color='Entity', color_discrete_map=entity_colors)
+            fig_2 = px.bar(approved_counts, x='Entity', y='Count_Approved', title=f'Approvals by Entity for {selected_function} Function',labels={'Entity': 'Entity', 'Count_Approved': 'Approvals'}, color='Entity')
             fig_2.update_layout(showlegend=False)
-            # Barchart 6: Unique_LCs by Function
-            # Get the count of 'Unique_LCs' related to each entity based on the selected function
-            # unique_lcs_counts = count_unique_lcs_by_entity(data, selected_function)
-
-            # # Create a bar chart using Plotly Express
-            # fig_3 = px.bar(unique_lcs_counts, x='Entity', y='Count_Unique_LCs', title=f'No of Unique_LCs by Entity for {selected_function} Function',labels={'Entity': 'Entity', 'Count_Unique_LCs': 'Unique LCs'}, color='Entity', color_discrete_map=entity_colors)
-            # fig_3.update_layout(showlegend=False)
             
-
-            # Display the bar charts using Plotly Chart
-            # col1, col2, col3 = st.columns(3)
-            # col1, col2 = st.columns(2)
-
-            # with col1:
-            #     # Render the bar chart using Streamlit
-            #     st.plotly_chart(fig_1, use_container_width=True)
-
-            # with col2:
-            #     # Render the bar chart using Streamlit
-            #     st.plotly_chart(fig_2, use_container_width=True)
             st.plotly_chart(fig_2, use_container_width=True)
             st.plotly_chart(fig_1, use_container_width=True)
             
-            # with col3:
-            #     # Render the bar chart using Streamlit
-            #     st.plotly_chart(fig_3, use_container_width=True)
-
+            
             st.write("<br><br>", unsafe_allow_html=True)
             #Footer
-            st.write("<p style='text-align: center;'>Made with ❤️ by &lt;/Dev.Team&gt; of AIESEC in Sri Lanka</p>", unsafe_allow_html=True)
+            st.write("<p style='text-align: center;'>Made with ❤️ by &lt;/Dev.Team&gt; of <strong>AIESEC in Sri Lanka</strong></p>", unsafe_allow_html=True)
 
 
         else:
